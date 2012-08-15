78086981508194bdadc7cdf9b68101b89dd4d4c0
sc <sc@ubuntu.ubuntu-domain>
2012-08-15 11:03:09 +0530

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Sat Aug 11 22:15:04 IST 2012"
+#define LINUX_COMPILE_TIME "22:15:04"
+#define LINUX_COMPILE_BY "nims"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "ubuntu-domain"
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
